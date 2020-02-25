704024c7642e315689054f046c38f95f47149239
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-02-25 00:38:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,10 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  characters = []
+  characters = [
+      { name:  "Pat Monteseian", occupation: "Staff" },
+      { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+      ]
 end
 
 def aoh_lookup(aoh, row, key)
